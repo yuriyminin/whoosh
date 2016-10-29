c10613465acaffae83edd5be106d9b7f50a437e8
Yuriy Minin <7yuriy7@gmail.com>
2016-10-29 16:48:24 -0500

    server done

--- app.js
+++ app.js
@@ -2,11 +2,31 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
 
 app.use(express.static('../src'));
+app.use(bodyParser.json());
+
+global.planes = [];
 
 app.get('/', function(req, res){
   res.sendfile('src/index.html');
+});
+
+app.get('/planes', function(req, res) {
+  res.send(global.planes);
+});
+
+app.get('/plane', function(req, res) {
+  var rand = global.planes[Math.floor(Math.random() * global.planes.length)];
+  console.log(rand);
+  res.send(JSON.stringify(rand));
+});
+
+app.post('/planes', function (req, res) {
+  global.planes.push([req.body.message,req.body.emotion]);
+  console.log(global.planes);
+  res.send('Success');
 });
 
 io.on('connection', function(socket){
