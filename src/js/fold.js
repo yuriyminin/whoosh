fefd2232addbc5354411ba28b43517554d1ab8b3
Ali de Jong <ali.dejong@utexas.edu>
2016-10-29 21:11:06 -0500

    view plane animation v not happy with me

--- src/js/fold.js
+++ src/js/fold.js
@@ -12,6 +12,7 @@ $().ready(function() {
                                 $('#container').addClass('fly_away_first');
                                 setTimeout(function() {
                                     $('#container').addClass('fly_away');
+                                    document.getElementById("planetext").value = '';
                                 }, 600);
                             }, 2000);
                         }, 2800);
@@ -20,12 +21,18 @@ $().ready(function() {
 
 
   $('#add').click(function() {
+            document.querySelector("#planetext").style.boxShadow = '0 0 10px #525354';
             $('#plate').addClass('front');
             $('#container').removeClass('fly_away fly_away_first hover').addClass('beginning');
             $('.curvable').removeClass('curved');
   });
 
   $('#get').click(function() {
-            console.log("Get a Plane");
+      document.querySelector("#planetext").style.boxShadow = 'none';
+      $('.message').attr('contenteditable', 'false');
+      console.log(document.getElementById("planetext").contentEditable);
+      $('#view').addClass('read');
+      $('#container').removeClass('fly_away fly_away_first hover').addClass('beginning');
+      $('.curvable').removeClass('curved');
           });
   });
