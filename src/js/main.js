20cbdc90e2ccd1758b81ab3f2b3795d332702cbb
NickySlicksHaha <nickengmann_2@yahoo.com>
2016-10-30 04:39:01 -0400

    Nick's changes 3am

--- src/js/main.js
+++ src/js/main.js
@@ -115,7 +115,7 @@
 })();
 
 $(".type-wrap span:last").typed({
-	strings: ["^1000 Write down how you are feeling right now..."],
+	strings: ["^1000 How are you feeling right now?"],
 	typeSpeed: 50,
 	loop: false,
 	contentType: 'html'
